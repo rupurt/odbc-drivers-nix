2e34274269b9c981d1a2fa8ecf23d3027edada10
Alex Kwiatkowski <alex+git@fremantle.io>
2023-09-04 15:25:33 -0700

    use libxcrypt-legacy for latest glibc

--- flake.nix
+++ flake.nix
@@ -2,28 +2,22 @@
   description = "Nix flake for ODBC drivers";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     flake-utils,
+    nixpkgs,
     ...
   }: let
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     outputs = flake-utils.lib.eachSystem systems (system: let
-      # use old version of nix packages that builds against glibc 2.35
-      pkgs =
-        import (builtins.fetchGit {
-          name = "nixpkgs-with-glibc-2.35-224";
-          url = "https://github.com/nixos/nixpkgs";
-          ref = "refs/heads/nixpkgs-unstable";
-          rev = "8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
-        }) {
-          inherit system;
-          overlays = [self.overlay];
-        };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [self.overlay];
+      };
     in {
       # packages exported by the flake
       packages = {
